ac92def43712ae867dd484484445374303ded452
eyvonne <eyvonne@users.noreply.github.com>
2020-04-10 12:25:29 -0700

    now name fields appear

--- FlaskApp/static/js/newMeeting.js
+++ FlaskApp/static/js/newMeeting.js
@@ -1,5 +1,17 @@
-$(document).ready(function(){
-  $("button").click(function(){
-    alert('WOW you changed the form')}
-  )}
-)
+$(function(){
+  $("#numAttendees").change(function(){
+      $("#nameEntry").html(function(){
+        html = ''
+        for (let attendee = 0; attendee<$("#numAttendees").val(); attendee++){
+          html = html.concat('<input class="form-control" type="text" placeholder="Enter Name">')
+        }
+        alert(html)
+        return html
+      })
+  })
+})
+
+
+var enter_names = `{{% for i in numAttend %}}
+  <input class="form-control" type="text" placeholder="Enter Name {i}">
+{{% endfor %}}`
