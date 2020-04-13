6426206dbc5672169c25d6e26330e71792654e54
eyvonne <eyvonne@users.noreply.github.com>
2020-04-13 16:50:05 -0700

    add user works

--- FlaskApp/static/js/mainMeeting.js
+++ FlaskApp/static/js/mainMeeting.js
@@ -1,4 +1,9 @@
 
   $("#addButton").click(function(){
-    alert('you clicked the button!')
-  })
+    $("#Meeting").append(function(){
+      newAttendee = $("#newAttendee").val()
+      html = `<label class="btn btn-secondary"><input type="radio" name="options" id="${newAttendee}" autocomplete="off"> ${newAttendee}: 00:00</label>`
+      $("#newAttendee").val("")
+      return html
+    })
+  });
